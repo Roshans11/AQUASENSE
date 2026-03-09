63aaef18065c346d34f974e2346ae3db06dee992
Roshans11 <singhroshanb1@gmail.com>
2026-03-09 05:53:31 +0530

    first commit

---
+++ FIREBASE.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAA6exHxfF7JXem702r3Wg7z47w5tmQfm4",
+  authDomain: "aquasense-c58a5.firebaseapp.com",
+  projectId: "aquasense-c58a5",
+  storageBucket: "aquasense-c58a5.firebasestorage.app",
+  messagingSenderId: "1085068394343",
+  appId: "1:1085068394343:web:bebfeef682433df7a6b9c2",
+  measurementId: "G-7E94W8VDR1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAA6exHxfF7JXem702r3Wg7z47w5tmQfm4",
+    authDomain: "aquasense-c58a5.firebaseapp.com",
+    projectId: "aquasense-c58a5",
+    storageBucket: "aquasense-c58a5.firebasestorage.app",
+    messagingSenderId: "1085068394343",
+    appId: "1:1085068394343:web:bebfeef682433df7a6b9c2",
+    measurementId: "G-7E94W8VDR1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
